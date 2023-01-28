daa572628edac078e89833fd83c0960f68963de4
Sam Barrido <ssbarrido@gmail.com>
2023-01-28 10:42:50 -0500

    test intelliJ config

--- assignments/a2/src/test/java/ArrayListTest.java
+++ assignments/a2/src/test/java/ArrayListTest.java
@@ -7,4 +7,9 @@ public class ArrayListTest {
     TODO: write test cases testing the accuracy of the methods marked with TODO. Write an efficiency test which asserts that the
     SortedArrayTest is faster than the ArrayList. Be sure to test for edge cases.
      */
+    @Test
+    void initTest() {
+        System.out.println("Hello");
+        assertEquals("Hello", "Hello");
+    }
 }
