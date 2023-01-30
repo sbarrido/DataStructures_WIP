6df0b275f638753fbaa50508780f1ad719b3bc67
Sam Barrido <ssbarrido@gmail.com>
2023-01-30 16:29:01 -0500

    Complete Node tests, implement Search Engine

--- assignments/a2/src/main/java/SearchEngine.java
+++ assignments/a2/src/main/java/SearchEngine.java
@@ -14,17 +14,18 @@ public class SearchEngine {
     public SearchEngine(int mode) throws IOException {
         //1. = ArrayList
         //2. = SortedArrayList
-        switch(mode) {
-            case 1:
+        switch (mode) {
+            case 1 -> {
                 this.nodeList = new ArrayList<>();
+                this.mode = mode;
                 this.buildList();
-                break;
-            case 2:
+            }
+            case 2 -> {
                 this.nodeList = new SortedArrayList<>();
+                this.mode = mode;
                 this.buildList();
-                break;
-            default:
-                throw new IOException();
+            }
+            default -> throw new IOException();
         }
     }
 
@@ -36,9 +37,12 @@ public class SearchEngine {
     // if it hasn't been seen. If the node has been created already, add the current URL to its references. Add the Node to the the
     // SearchEngine's nodeList
     public void buildList() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("dataset.txt"));
+        InputStream in = getClass().getClassLoader().getResourceAsStream("dataset.txt");
+        InputStreamReader inRead = new InputStreamReader((in));
+        BufferedReader reader = new BufferedReader(inRead);
         String url;
         while((url = reader.readLine()) != null){
+//            System.out.println("Loading " + url);
             Document doc = Jsoup.connect(url).get();
             String text = doc.body().text().toLowerCase();
             String[] words = text.split("\\s+"); // splits by whitespace
@@ -65,20 +69,27 @@ public class SearchEngine {
 
     // TODO: Return the node's reference list - if the term isn't found, return an empty list
     public List<String> search(String term) {
-        System.out.println("Searching for " + term + " using data structure mode " + mode + "...");
+        long startTime = System.nanoTime();
+        System.out.println("Searching for " + term + " using data structure mode " + this.mode + "...");
         // Search logic goes here
         Node targetNode = new Node(term, this.mode);
         int flag = this.nodeList.search(targetNode);
         List<String> target = null;
-        if(flag != -1 ){
+        if (flag != -1) {
             target = this.nodeList.get(flag).getReferences();
         }
+        long endTime = System.nanoTime();
         // Example code for displaying results
 //        System.out.println("Displaying results for " + term + ":");
 //        System.out.println("    1. URL 1: ");
 //        System.out.println("    2. URL 2: ");
 //        System.out.println("    3. URL 3: ");
-        System.out.print(target);
+        long time = endTime - startTime;
+        System.out.println("Found " + target.size() + " results in " + time + "nanoseconds");
+        System.out.println("Displaying results for '" + term + "':");
+        for(int i = 0; i < target.size(); i++) {
+            System.out.println(i + ". " + target.get(i));
+        }
         return target;
     }
 
--- assignments/a2/src/test/java/NodeTest.java
+++ assignments/a2/src/test/java/NodeTest.java
@@ -1,9 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NodeTest {
+    private static Node n1;
+    private static Node n2;
+    private static Node s1;
         /*
     TODO: write test cases testing the accuracy of the methods marked with TODO.s.
      */
+    @BeforeEach
+    void initNode() {
+        n1 = new Node("banana", 1);
+        n2 = new Node("yum", 1);
+        s1 = new Node("banana" , 1);
+    }
+
+    @Test
+    void initTest() {
+        assertEquals("banana", n1.getKeyword());
+        assertEquals(true, n1.equals(s1));
+        assertEquals(false, n1.equals(n2));
+        assertEquals(true, n1.compareTo(s1) == 0);
+        assertEquals(true, n1.compareTo(n2) > 0);
+    }
 }
