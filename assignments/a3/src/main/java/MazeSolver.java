0287936ce0cc00f01a1a065266beca76e9af2806
Sam Barrido <ssbarrido@gmail.com>
2023-02-06 23:45:21 -0500

    Complete maze solver

--- assignments/a3/src/main/java/MazeSolver.java
+++ assignments/a3/src/main/java/MazeSolver.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +6,9 @@ import java.util.List;
 
 public class MazeSolver {
     static char[][] maze;
+    static Boolean[][] tTable;
+    static Stack<int[]> stack;
+    static Queue<int[]> queue;
     static int m, n; // dimensions of the maze
 
     /*
@@ -17,16 +18,83 @@ public class MazeSolver {
     to the first line of input. Then it fills the maze with the maze from the file.
      */
     public static void setMaze(String file) throws IOException {
+        InputStream in = MazeSolver.class.getClassLoader().getResourceAsStream(file);
+        InputStreamReader inRead = new InputStreamReader((in));
+        BufferedReader reader = new BufferedReader(inRead);
+        //Creat and read lines into buffer
+        /*BufferedReader buffRd = new BufferedReader(new FileReader(file));*/
+        String[] initMazeProp = reader.readLine().split(" ");
 
+        //First line always m,n of maze
+        //initialize size of maze
+        //initialize truth table
+        MazeSolver.m = Integer.parseInt(initMazeProp[0]);
+        MazeSolver.n = Integer.parseInt(initMazeProp[1]);
+        MazeSolver.maze = new char[MazeSolver.m][MazeSolver.n];
+        MazeSolver.tTable = new Boolean[MazeSolver.m][MazeSolver.n];
+        //Store buffered lines into maze
+        int row = 0;
+        int col = 0;
+        while(reader.ready() && row < MazeSolver.m) {
+            String line = reader.readLine();
+            char[] chars = line.toCharArray();
+            for(col = 0; col < chars.length; col++) {
+                MazeSolver.maze[row][col] = chars[col];
+                MazeSolver.tTable[row][col] = MazeSolver.tHelper(chars[col]);
+            }
+            row++;
+        }
     }
 
+    //Helper Function for Truth Table
+    //Boolean values returned can be either null or false
+    //null = wall, false = not visited
+    public static Boolean tHelper(char val) {
+        Boolean target = null;
+        if(val == 'S' || val == '.' || val == 'G') {
+            target = false;
+        }
+
+        return target;
+    }
     /*
     TODO: isValid - checks if a position on the board has not been visited and is within bounds
      */
     public static boolean isValid(int x, int y) {
-        return false;
-    }
+        //Boundary
+        boolean lowerBound = x < 0 || y < 0;
+        boolean upperBound = x >= MazeSolver.m || y >= MazeSolver.n;
+        if(lowerBound | upperBound) {
+            return false;
+        }
 
+        //CheckTruth Table
+        //null = wall = not valid
+        //false = 'S', 'G', ',' = valid path
+        boolean target = false;
+        if(MazeSolver.tTable[x][y] == null) {
+            target = false;
+        } else {
+            if(MazeSolver.tTable[x][y] == false) {
+                target = true;
+            }
+        }
+        return target;
+    }
+    //Helper function to find starting posn, 'S'
+    public static int[] findStart() {
+        int[] target = new int[2];
+        for(int i = 0; i < MazeSolver.maze.length; i++) {
+            for(int j = 0; j < MazeSolver.maze[i].length; j++) {
+                if(MazeSolver.maze[i][j] == 'S') {
+                    target[0] = i;
+                    target[1] = j;
+                    break;
+                }
+            }
+        }
+        return target;
+    }
 
     /*
     TODO: Using a stack, solve the maze WITHOUT recursion.
@@ -42,11 +110,72 @@ public class MazeSolver {
      */
 
     public static boolean solveMazeStack(int x, int y)  throws EmptyStackE {
-        return false;
+        //Init variables and starting posn in stack
+        boolean isGoal = false;
+        int[] startPosn = {x, y};
+        stack = new Stack<>();
+        stack.push(startPosn);
+
+        //while stack is not empty
+        while(stack.size() > 0) {
+
+            //Pop stack and check if End, 'G'
+            int[] curr = stack.pop();
+            int cx, cy;
+            cx = curr[0];
+            cy = curr[1];
+
+            if(maze[cx][cy] == 'G') {
+                return true;
+            } else {
+                //tTable visited [][]
+                //used in isValid
+                tTable[cx][cy] = true;
+
+                //Checks if Neighbor nodes valid
+                //true: push onto stack
+                if(isValid(cx-1, cy)) stack.push(new int[] {cx-1, cy});
+                if(isValid(cx+1, cy)) stack.push(new int[] {cx+1, cy});
+                if(isValid(cx, cy-1)) stack.push(new int[] {cx, cy-1});
+                if(isValid(cx, cy+1)) stack.push(new int[] {cx, cy+1});
+            }
+        }
+        return isGoal;
     }
 
     // TODO: Using a queue, solve the maze. Be sure to explain your algorithm for full points.
     public static boolean solveMazeQueue(int x, int y) throws EmptyQueueE{
+        //init vars and starting posn for queue
+        boolean isGoal = false;
+        int[] startPosn = {x, y};
+        queue = new Queue<>();
+        queue.enqueue(startPosn);
+
+        //loop while not empty
+        while(queue.size() > 0 ) {
+            //deque
+            int[] curr = queue.dequeue();
+            int cx, cy;
+            cx = curr[0];
+            cy = curr[1];
+
+            //check if value == 'G'
+            //else, check neighbor nodes
+            //valid neigbor nodes enqueued
+            // 1. not visited
+            // 2. not wall/edge
+            if(maze[cx][cy] == 'G') {
+                return true;
+            } else {
+                tTable[cx][cy] = true;
+
+                if(isValid(cx-1, cy)) queue.enqueue(new int[] {cx-1, cy});
+                if(isValid(cx+1, cy)) queue.enqueue(new int[] {cx+1, cy});
+                if(isValid(cx, cy-1)) queue.enqueue(new int[] {cx, cy-1});
+                if(isValid(cx, cy+1)) queue.enqueue(new int[] {cx, cy+1});
+            }
+        }
+
         return false;
     }
 
@@ -56,9 +185,18 @@ public class MazeSolver {
     public static boolean solve(String file, int mode) throws IOException, EmptyStackE, EmptyQueueE {
         // find starting point
         // check if the maze can be solved
-        boolean solved = false;
+        setMaze(file);
+        int[] startPosn = findStart();
+
+        boolean solved;
+        if(mode == 1) {
+            solved = solveMazeStack(startPosn[0], startPosn[1]);
+        } else {
+            solved = solveMazeQueue(startPosn[0], startPosn[1]);
+        }
+
         System.out.println("Maze can be solved: " + solved);
-        return false;
+        return solved;
     }
 
 
--- assignments/a3/src/test/java/MazeSolverTest.java
+++ assignments/a3/src/test/java/MazeSolverTest.java
@@ -4,7 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MazeSolverTest {
     @Test
-    void helloTest() {
-        System.out.println("hello");
+    void mazeStackTest() {
+        try{
+            assertEquals(true, MazeSolver.solve("maze1.txt", 1));
+            assertEquals(true, MazeSolver.solve("maze2.txt", 1));
+            assertEquals(false, MazeSolver.solve("maze3.txt", 1));
+
+            assertEquals(true, MazeSolver.solve("maze1.txt", 2));
+            assertEquals(true, MazeSolver.solve("maze2.txt", 2));
+            assertEquals(false, MazeSolver.solve("maze3.txt", 2));
+        }catch(Exception ex) {
+            System.out.println(ex);
+        }
     }
 }
